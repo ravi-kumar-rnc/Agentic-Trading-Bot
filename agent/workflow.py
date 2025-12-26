bee345968af388b0e8014f7493bdaa4c2b6971a3
ravi-kumar-rnc <ravi.kumar.rnc@gmail.com>
2025-12-26 12:55:55 +0530

    workflow.py added

---
+++ agent/workflow.py
@@ -0,0 +1,27 @@
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
+from typing import Annotated, TypedDict
+from langgraph.graph.message import add_messages
+from langchain_core.messages import HumanMessage, SystemMessage
+from logs.logger import logger
+
+from toolkit.tools import *
+
+class StateGraph(TypedDict):
+    messages=Annotated[list, add_messages]
+
+class graph_builder:
+    def __init__(self):
+        pass
+
+    def _chatbot_node(self, state: StateGraph):
+        pass
+    
+    def build(self):
+        pass
+
+    def get_graph(Self):
+        pass
+    
+    
+
